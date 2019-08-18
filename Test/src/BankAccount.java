bcf1ef5dc679844a0161a1ed9484a7e894f73a5e
Imran Hussain <imran.hussain@evampsaanga.com>
2019-08-18 06:24:29 +0500

    Builder Pattern

---
+++ Test/src/BankAccount.java
@@ -0,0 +1,82 @@
+public class BankAccount {
+    private String name;
+    private String accountNumber;
+    private String email;
+    private boolean newsletter;
+
+    //The constructor that takes a builder from which it will create object
+    //the access to this is only provided to builder
+    private BankAccount(BankAccountBuilder builder) {
+        this.name = builder.name;
+        this.accountNumber = builder.accountNumber;
+        this.email = builder.email;
+        this.newsletter = builder.newsletter;
+    }
+
+    public static class BankAccountBuilder {
+        private String name;
+        private String accountNumber;
+        private String email;
+        private boolean newsletter;
+
+        //All Mandatory parameters goes with this constructor
+        public BankAccountBuilder(String name, String accountNumber) {
+            this.name = name;
+            this.accountNumber = accountNumber;
+        }
+
+        //setters for optional parameters which returns this same builder
+        //to support fluent design
+        public BankAccountBuilder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public BankAccountBuilder wantNewsletter(boolean newsletter) {
+            this.newsletter = newsletter;
+            return this;
+        }
+
+        //the actual build method that prepares and returns a BankAccount object
+        public BankAccount build() {
+            return new BankAccount(this);
+        }
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isNewsletter() {
+        return newsletter;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("BankAccount{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", accountNumber='").append(accountNumber).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", newsletter=").append(newsletter);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        BankAccount newAccount = new BankAccount
+                .BankAccountBuilder("Jon", "22738022275")
+
+
+                .build();
+        System.out.println(newAccount.toString());
+    }
+}
